2e98829d975951de1ec4ff47bacf7b8eeb0836ef
umidovalimardon06 <umidovalimardon06@gmail.com>
2024-11-30 23:09:35 +0500

    add Begin1

---
+++ Begin1/Main/src/Main.java
@@ -0,0 +1,21 @@
+// Umidov Alimardon.
+import java.util.Scanner ;
+public class Main {
+    // Kvadrat perimetrini topuvchi dastur.
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+         float a;
+         final float P;
+
+        System.out.print("Kvadratning a tomonini kiriting:"); // Input savol
+        a = in.nextFloat();    // Inputni o'qish.
+        System.out.println("qabul qilindi☑️");
+
+        P = 4*a;     // Asosiy expression.
+
+        System.out.println("Perimetr: "+P);
+
+
+
+    }
+}

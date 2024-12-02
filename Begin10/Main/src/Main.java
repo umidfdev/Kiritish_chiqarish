bf4eb44773d8039c59a45994f56322c73a255280
umidovalimardon06 <umidovalimardon06@gmail.com>
2024-12-03 00:31:04 +0500

    add Begin10

--- Begin10/Main/src/Main.java
+++ Begin10/Main/src/Main.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
+
 // Umidov Alimardon.
 class Main {
     // sonlar !=0 ular yig'indisi,ko'paytmasi va har biri kvsi.
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in); // Scanner obyekti.
 
-        float a,b;
+        float a, b;
+
 
         System.out.print("1-sonni kiriting:");
         a = in.nextFloat();
@@ -21,13 +23,10 @@ class Main {
         }
 
 
-
-        System.out.println("yig'indi: "+ (a+b) ); // Yig'indisi.
-        System.out.println("ayirma: "+ (Math.abs(a-b))); // Ayirmasi.
-        System.out.println("1-son kvsi: "+ Math.pow(a,2)); // 1-kvadrati.
-        System.out.println("2-son kvsi: "+ Math.pow(b,2)); // 2-kvadrati.
-
-
+        System.out.println("yig'indi: " + (a + b)); // Yig'indisi.
+        System.out.println("ayirma: " + (Math.abs(a - b))); // Ayirmasi.
+        System.out.println("1-son kvsi: " + Math.pow(a, 2)); // 1-kvadrati.
+        System.out.println("2-son kvsi: " + Math.pow(b, 2)); // 2-kvadrati.
 
 
     }

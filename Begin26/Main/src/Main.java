188e2ab9335beb16a997a69a6fdf8f17b13267da
umidovalimardon06 <umidovalimardon06@gmail.com>
2024-12-05 05:36:35 +0500

    add Begin25

---
+++ Begin26/Main/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+// Umidov Alimardon.
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        float x;
+
+        System.out.print("x-ni kiriting:");
+        x = in.nextFloat();
+
+        System.out.println("Natija: "+(4*(Math.pow(x-3,6)) -7*(Math.pow((x-3),3)) +2));
+
+
+
+
+    }
+}
